67ee89605eb534bfb61fa77c113b96267bee7e35
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-01-06 19:02:34 +0300

    Написал решение задачи 5 по второму семинару Python.

--- Task25.py
+++ Task25.py
@@ -4,3 +4,15 @@ import os
 import random
 os.system('cls')
 
+import os
+import random
+os.system('cls')
+
+n = int(input('Введите число N: '))
+
+my_list = []
+for _ in range(n):
+    my_list.append(random.randint(-n, n))
+print('Полученный список из N-элементов:', my_list)
+random.shuffle(my_list)
+print('Переменный случайным образом список из N-элементов:', my_list)
